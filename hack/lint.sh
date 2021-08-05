c3a8cad58aac8cb4fbe160c970f6dcbb081dcbd5
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-08-05 11:46:47 -0700

    Fix shellcheck version
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- hack/lint.sh
+++ hack/lint.sh
@@ -13,7 +13,9 @@ readonly root
 # DL3059 is about not having multiple successive RUN statements, and this is moronic
 # SC2039 is about array ref in POSIX shells (we are using bash, so)
 # SC2027 is about quotes inside quotes, and is moronic too
-readonly hadolint_ignore=(--ignore DL3006 --ignore DL3029 --ignore DL4006 --ignore DL3059 --ignore SC2039 --ignore SC2027)
+
+# XXX For some hard to fathom reason, the CI reports errors that the local test does not - specifically SC3014 and SC3054, so, also ignoring these since we use bash
+readonly hadolint_ignore=(--ignore DL3006 --ignore DL3029 --ignore DL4006 --ignore DL3059 --ignore SC2039 --ignore SC2027 --ignore SC3014 --ignore SC3054)
 
 if ! hadolint "${hadolint_ignore[@]}" "$root"/*Dockerfile*; then
   printf >&2 "Failed linting on Dockerfile\n"

d1a5debc6e23fbc616e7f48d9a220148c2831787
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-08-05 16:08:26 -0700

    More sc fuckerism
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- hack/lint.sh
+++ hack/lint.sh
@@ -14,8 +14,8 @@ readonly root
 # SC2039 is about array ref in POSIX shells (we are using bash, so)
 # SC2027 is about quotes inside quotes, and is moronic too
 
-# XXX For some hard to fathom reason, the CI reports errors that the local test does not - specifically SC3014 and SC3054, so, also ignoring these since we use bash
-readonly hadolint_ignore=(--ignore DL3006 --ignore DL3029 --ignore DL4006 --ignore DL3059 --ignore SC2039 --ignore SC2027 --ignore SC3014 --ignore SC3054)
+# XXX For some hard to fathom reason, the CI reports errors that the local test does not - specifically SC3014 SC3054 SC3010, so, also ignoring these since we use bash
+readonly hadolint_ignore=(--ignore DL3006 --ignore DL3029 --ignore DL4006 --ignore DL3059 --ignore SC2039 --ignore SC2027 --ignore SC3014 --ignore SC3054 --ignore SC3010)
 
 if ! hadolint "${hadolint_ignore[@]}" "$root"/*Dockerfile*; then
   printf >&2 "Failed linting on Dockerfile\n"
